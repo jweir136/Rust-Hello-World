59880b52a78012fe7347c8afa130357243d2a82b
jweir136 <jweir136@yahoo.com>
2022-02-15 15:05:04 -0500

    add docs

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,9 @@
 #![allow(non_snake_case)]
 
+/**
+ * This is the main function. The code starts running from here.
+ * All this code does is output "Hello World!" to the screen.
+ */
 fn main() {
     println!("Hello World!");
 }
